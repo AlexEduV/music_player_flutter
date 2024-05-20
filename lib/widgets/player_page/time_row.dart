bade80963454be8d3e6abc2be686a2c9ef59461b
Alexander <m.mirvua@gmail.com>
2024-05-20 09:30:50 +0300

    reworked time row widget in player page;

--- lib/widgets/player_page/time_row.dart
+++ lib/widgets/player_page/time_row.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:music_player_flutter/model/model.dart';
-import 'package:music_player_flutter/model/song.dart';
 
 class TimeRow extends StatelessWidget {
 
@@ -15,25 +14,33 @@ class TimeRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    TextStyle timeStyle = TextStyle(color: Colors.grey[200]);
 
-    DataModel model = context.watch<DataModel>();
-    Song openedSong = model.getSongById(openedSongIndex);
-
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            openedSong.currentTime,
-            style: TextStyle(color: Colors.grey[200]),
-          ),
-
-          Text(
-            openedSong.maxTime,
-            style: TextStyle(color: Colors.grey[200]),
-          ),
-        ],
+    return Consumer<DataModel>(
+      builder: (context, model, child) =>
+      Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Consumer<DataModel>(
+              builder: (context, model, child) =>
+                  Text(
+                    model.getSongById(openedSongIndex).currentTime,
+                    style: timeStyle,
+                  ),
+                ),
+      
+            Consumer<DataModel>(
+              builder: (context, model, child) =>
+                Text(
+                  model.getSongById(openedSongIndex).currentTime,
+                  style: timeStyle,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
