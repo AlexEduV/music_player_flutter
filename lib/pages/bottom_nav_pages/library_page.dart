975b05208f9b2285f0dd75c311bbfa36d627c666
Alexander <m.mirvua@gmail.com>
2024-06-13 13:38:43 +0300

    fixed file extension uri incompatibility;

--- lib/pages/bottom_nav_pages/library_page.dart
+++ lib/pages/bottom_nav_pages/library_page.dart
@@ -121,7 +121,7 @@ class _LibraryPageState extends State<LibraryPage> {
               artist: audio.artist ?? '',
               album: audio.album ?? '',
               maxTime: getTimeStringFromDouble((audio.duration ?? 0.0) / 1000),
-              source:'${audio.uri}.${audio.fileExtension}',
+              source:'${audio.uri}',
               isStatic: false,
             )
         );
