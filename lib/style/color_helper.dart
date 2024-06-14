295919683820e3c2744adc3e33203d44ac409696
Alexander <m.mirvua@gmail.com>
2024-06-14 15:01:20 +0300

    changed app color to blue;

--- lib/style/color_helper.dart
+++ lib/style/color_helper.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
 class ColorHelper {
-  static const Color mainColor = Color(0xff462276);
-  static const Color mainLighterColor = Color(0xff7A558C);
 
-
+  static const Color mainColor = Color(0xff224476);
+  static const Color mainLighterColor = Color(0xFF375D98);
+  
 }
